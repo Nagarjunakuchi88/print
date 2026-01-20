4da95998e1ee35eb2c6c0d9e41f46015ea0aae63
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-01-20 22:18:58 +0530

    Update PrintServiceImpl.java

--- src/main/java/io/mosip/print/service/impl/PrintServiceImpl.java
+++ src/main/java/io/mosip/print/service/impl/PrintServiceImpl.java
@@ -343,9 +343,11 @@ public class PrintServiceImpl implements PrintService {
                 attributes.put("isPhotoSet", isPhotoSet);
             }
             uin = decryptedJson.getString("UIN");
+			String uid = decryptedJson.getString("UID");
 
             setTemplateAttributes(decryptedJson.toString(), attributes);
             attributes.put(IdType.UIN.toString(), uin);
+			attributes.put(IdType.UIN.toString(), uid);
             attributes.put(IdType.RID.toString(), registrationId);
 
             String prefLangAttr = (String) attributes.get(userPreferredLanguageAttribute);
